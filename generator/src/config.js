85d01ea31c1040fab60656028312017bc14e7f4f
bgastaldo <61537901+bgastaldo@users.noreply.github.com>
2022-02-21 17:19:26 -0500

    upload

---
+++ generator/src/config.js
@@ -0,0 +1,126 @@
+const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.sol;
+
+// General metadata for Ethereum
+const namePrefix = "Universols";
+const description = "Universols vert cool description here";
+const baseUri = "ipfs://NewUriToReplace";
+
+const solanaMetadata = {
+  symbol: "UNIVERSOLS",
+  seller_fee_basis_points: 800, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://www.universols.io/",
+  creators: [
+    {
+      address: "C9cWha3XXATr4WT1xZ1M7pVLGJWvPEnWcssWCLWdUszA",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 1100,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Wings" },
+      { name: "Glass Color" },
+      { name: "Skin Color" },
+      { name: "Body" },
+      { name: "Badge" },
+      { name: "Eyes" },
+      { name: "Hair Color" },
+      { name: "Hats" },
+      { name: "Mouth" },
+      { name: "Nose" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = true;
+
+const format = {
+  width: 1000,
+  height: 1000,
+  smoothing: false,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: false,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+};
